28649c514566a57486cf41b9bbb17db2317df51d
Wicke Sebastian <sebastian.wicke@elektrobit.com>
2023-02-10 14:04:43 +0100

    Filter and Mail
    new Filteraction and mailing possible now

--- jtc_mail.py
+++ jtc_mail.py
@@ -3,10 +3,11 @@ import smtplib
 import ssl
 import jtc_start
 import jtc_resource
+from email import mime
 
-SMTP_SERVER = "localhost"
-PORT = 1025
-SENDER_EMAIL = "localhost"
+SMTP_SERVER = "mail-de.ebgroup.elektrobit.com"
+PORT = 587
+SENDER_EMAIL = "JTC-update-mail"
 
 inform_me = jtc_start.run_jtc()
 
@@ -14,60 +15,34 @@ def send_mail():
     '''send a mail when alt least 1 ticket has been created else send a mail when no ticket has been created'''
     # Try to log in to server and send email
     if (len(inform_me)) > 0:
-        # Create a secure SSL context
-        context = ssl.create_default_context()
         # Try to log in to server and send email
-        try:
-            server = smtplib.SMTP(jtc_resource.smtp_server["smtp"], jtc_resource.port)
-            server.ehlo()
-            server.starttls(context=context)
-            server.ehlo()
-            server.login(jtc_resource.sender_email["sender"], jtc_resource.pw["pw"])
-            message = """\
+        smtp = smtplib.SMTP("mail-de.ebgroup.elektrobit.com", 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(jtc_resource.sender_email("sender"), jtc_resource.mail_pw("mail_pw"))
+        msg1 = """\
                 Subject: Report JTC - Automization
                 Hi {assignee},\n\n
-                The following new Tickets have been created:\n
+                These new Tickets have been created:\n
                 {issue}\n"""
-
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(jtc_resource.smtp_server["smtp"],
-                                jtc_resource.port, context=context) as server:
-                server.login(jtc_resource.sender_email["sender"], jtc_resource.pw["pw"])
-                server.sendmail(jtc_resource.sender_email["sender"],
-                                jtc_resource.receiver_email["receiver"], message)
-        except Exception as exception:
-            exception = "Oder auch nicht"
-            print(exception)
-        finally:
-            server.quit()
+        smtp.sendmail(jtc_resource.sender_email("sender"), jtc_resource.receiver_email("receiver"), msg1)
+        smtp.close()
+        
 
     else:
-        '''send a mail when no ticket has been created'''
-        # Create a secure SSL context
-        context = ssl.create_default_context()
         # Try to log in to server and send email
-        try:
-            server = smtplib.SMTP(jtc_resource.smtp_server["smtp"], jtc_resource.port)
-            server.ehlo()
-            server.starttls(context=context)
-            server.ehlo()
-            server.login(jtc_resource.sender_email["sender"], jtc_resource.pw["pw"])
-            message = """\
+        smtp = smtplib.SMTP("mail-de.ebgroup.elektrobit.com", 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(jtc_resource.NB_USER, jtc_resource.mail_pw)
+        msg2 = """\
                 Subject: Report JTC - Automization
                 Hi {assignee},\n\n
                 No new Tickets have been created.\n
-                {issue}\n"""
-
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(jtc_resource.smtp_server["smtp"],
-                                jtc_resource.port, context=context) as server:
-                server.login(jtc_resource.sender_email["sender"], jtc_resource.pw["pw"])
-                server.sendmail(jtc_resource.sender_email["sender"],
-                                jtc_resource.receiver_email["receiver"], message)
-        except Exception as exception:
-            exception = "Oder auch nicht"
-            print(exception)
-        finally:
-            server.quit()
+                """
+        smtp.sendmail(jtc_resource.sender_email, jtc_resource.receiver_email, msg2)
+        smtp.close()
 
 send_mail()
---
+++ jtc_resource.py
@@ -0,0 +1,8 @@
+usr = {"usr": "sewi273376"}
+pw = {"pw": "bdkn14130522???"}
+mail_pw = "Fza9HxY8u1a3r"
+smtp_server = "mail-de.ebgroup.elektrobit.com"
+port = 587
+sender_email = "JTC-update-mail@elektrobit.com"
+receiver_email = "sebastian.wicke@elektrobit.com"
+NB_USER = "ebgroup\JTC-update-mail"
